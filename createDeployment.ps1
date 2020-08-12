9861c904e11bab0cd25544733fa616ca7c4d38fe
Andy Hopper <andyhopp@amazon.com>
2020-08-11 22:05:42 -0400

    Corrected zip layout to match appSpec.yml

--- createDeployment.ps1
+++ createDeployment.ps1
@@ -2,7 +2,8 @@ dotnet publish -c Release
 if (!(Test-Path deployment)) { mkdir deployment }
 pushd deployment
 cp -Force -Recurse ..\CodeDeploy\* .
-cp -Force -Recurse ..\bin\Release\netcoreapp3.1\publish\
+if (!(Test-Path app)) { mkdir app }
+cp -Force -Recurse ..\bin\Release\netcoreapp3.1\publish\* app\
 if (Test-Path deployment.zip) { rm deployment.zip }
 Compress-Archive -DestinationPath .\deployment.zip -Path *
 popd
